58f95d2c77a2b2d73d82052e27aa472ef85e9f40
MiKU <2021100241@ms.bulsu.edu.ph>
2025-09-30 10:53:38 +0800

    Initial commit

---
+++ src/main/java/io/github/aronangeles/athletika/AthletikaApplication.java
@@ -0,0 +1,13 @@
+package io.github.aronangeles.athletika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthletikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthletikaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/aronangeles/athletika/AthletikaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aronangeles.athletika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthletikaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
