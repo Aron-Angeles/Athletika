95cb436790d51f122c0d5886dea348d16b4ee321
MiKU <2021100241@ms.bulsu.edu.ph>
2025-11-24 15:15:43 +0800

    fix: Workout not found exception not throwing

--- src/main/java/io/github/aronangeles/athletika/exceptions/GlobalExceptionHandler.java
+++ src/main/java/io/github/aronangeles/athletika/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package io.github.aronangeles.athletika.exceptions;
 import io.github.aronangeles.athletika.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,8 +19,4 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()));
     }
 
-//    @ExceptionHandler(HttpMessageNotReadableException.class)
-//    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ErrorMessages.INVALID_WORKOUT.getMessage()));
-//    }
 }
