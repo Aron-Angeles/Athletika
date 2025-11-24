a832354415d829c385d4589cfa9c01fe16498bf0
MiKU <2021100241@ms.bulsu.edu.ph>
2025-11-24 15:15:24 +0800

    fix: Workout not found exception not throwing

--- src/main/java/io/github/aronangeles/athletika/exceptions/GlobalExceptionHandler.java
+++ src/main/java/io/github/aronangeles/athletika/exceptions/GlobalExceptionHandler.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(WorkoutNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleWorkoutNotFoundException(WorkoutNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage()));
+    }
+
     @ExceptionHandler(WorkoutNotValidException.class)
     public ResponseEntity<ErrorResponse> handleWorkoutNotValidException(WorkoutNotValidException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()));
--- src/main/java/io/github/aronangeles/athletika/repositories/WorkoutRepository.java
+++ src/main/java/io/github/aronangeles/athletika/repositories/WorkoutRepository.java
@@ -1,6 +1,7 @@
 package io.github.aronangeles.athletika.repositories;
 
 import io.github.aronangeles.athletika.model.Workout;
+import io.github.aronangeles.athletika.model.WorkoutFocus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,8 +11,6 @@ import java.util.List;
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout,Integer> {
 
-    @Query("SELECT x FROM Workout x where x.focus = ?1")
-    List<Workout> findByFocus(String focus);
-
-
+    @Query("SELECT x FROM Workout x WHERE x.focus = :focus")
+    List<Workout> findByFocus(WorkoutFocus focus);
 }
