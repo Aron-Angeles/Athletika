1bd08204f29881bfd59f27ac2a3328011d2399a6
MiKU <2021100241@ms.bulsu.edu.ph>
2025-11-24 14:05:55 +0800

    fix: Enum array being passed on SQL

--- src/test/java/io/github/aronangeles/athletika/CreateWorkoutServiceTest.java
+++ src/test/java/io/github/aronangeles/athletika/CreateWorkoutServiceTest.java
@@ -1,13 +1,11 @@
 package io.github.aronangeles.athletika;
 
 import io.github.aronangeles.athletika.dto.WorkoutDTO;
-import io.github.aronangeles.athletika.exceptions.WorkoutNotValidException;
 import io.github.aronangeles.athletika.model.Workout;
 import io.github.aronangeles.athletika.model.WorkoutFocus;
 import io.github.aronangeles.athletika.model.WorkoutNames;
 import io.github.aronangeles.athletika.repositories.WorkoutRepository;
 import io.github.aronangeles.athletika.services.CreateWorkoutService;
-import io.github.aronangeles.athletika.validators.WorkoutValidator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,7 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
